6f3ba682f42f8bf6cb420d7a4a321179d97b7c84
Josue Lubaki <josuelubaki@gmail.com>
2022-05-11 17:50:06 -0400

    organization files & arrow function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
-function Expenses(props) {
+const Expenses = (props) => {
 	return (
 		<Card className='expenses'>
 			{props.items.map((item) => (
@@ -16,6 +16,6 @@ function Expenses(props) {
 			))}
 		</Card>
 	);
-}
+};
 
 export default Expenses;
