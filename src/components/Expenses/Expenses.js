5109d0059522910cdd3f28cfebab78c208e66802
Josue Lubaki <josuelubaki@gmail.com>
2022-05-12 14:09:43 -0400

    add ExpensesFilter component with Two ways Binding

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,31 @@
+import React, { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2020');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
 	return (
-		<Card className='expenses'>
-			{props.items.map((item) => (
-				<div key={item.id}>
-					<ExpenseItem
-						title={item.title}
-						amount={item.amount}
-						date={item.date}
-					/>
-				</div>
-			))}
-		</Card>
+		<div>
+			<Card className='expenses'>
+				<ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear} />
+				{props.items.map((item) => (
+					<div key={item.id}>
+						<ExpenseItem
+							title={item.title}
+							amount={item.amount}
+							date={item.date}
+						/>
+					</div>
+				))}
+			</Card>
+		</div>
 	);
 };
 
